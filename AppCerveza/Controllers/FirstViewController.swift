701acfd4920c3f81eb6f11285e6e665e42f287d3
María González García <mariaglezg@usal.es>
2021-12-20 20:55:11 +0100

    FirstView without data (icon included) committed 2.0

--- AppCerveza/Controllers/FirstViewController.swift
+++ AppCerveza/Controllers/FirstViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var etiqueta = UILabel()
     var tv = UITableView()
@@ -37,14 +37,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func prepararTableView() {
         tv.translatesAutoresizingMaskIntoConstraints = false
-        //tv = UITableView(frame: UIScreen.main.bounds, style: .plain)
         tv.backgroundColor = .white
         tv.delegate = self
         tv.dataSource = self
         tv.register(UITableViewCell.self, forCellReuseIdentifier: "fabricante")
     }
     
-    @nonobjc func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
     
@@ -52,7 +51,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 4
     }
     
-    @objc(tableView:cellForRowAtIndexPath:) func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    @objc func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "fabricante")
         
         cell?.contentView.backgroundColor = .systemBrown
@@ -64,11 +63,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell!
     }
     
-    @nonobjc func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 0
         {
             return "Fabricantes nacionales"
-                    }
+        }
         else
         {
             return "Fabricantes extranjeros"
@@ -79,53 +78,56 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         botonAdd.translatesAutoresizingMaskIntoConstraints = false
         botonAdd.backgroundColor = .lightGray
-        botonAdd.setTitle("Añadir Fabricante", for: .normal)
+        botonAdd.setTitle(" Añadir Fabricante ", for: .normal)
         botonAdd.setTitleColor(.blue, for: .normal)
         botonAdd.contentHorizontalAlignment = .center
-        botonAdd.addTarget(self, action: #selector(ViewController.botonAnadir), for: .touchUpInside)
+        botonAdd.layer.cornerRadius = 4
+        botonAdd.addTarget(self, action: #selector(FirstViewController.botonAnadir), for: .touchUpInside)
         
         botonDelete.translatesAutoresizingMaskIntoConstraints = false
         botonDelete.backgroundColor = .lightGray
-        botonDelete.setTitle("Eliminar Fabricante", for: .normal)
+        botonDelete.setTitle("  Eliminar Fabricante  ", for: .normal)
         botonDelete.setTitleColor(.blue, for: .normal)
         botonDelete.contentHorizontalAlignment = .center
-        botonDelete.addTarget(self, action: #selector(ViewController.botonEliminar), for: .touchUpInside)
+        botonDelete.layer.cornerRadius = 4
+        botonDelete.addTarget(self, action: #selector(FirstViewController.botonEliminar), for: .touchUpInside)
     }
     
     func prepararEtiqueta() {
         etiqueta.translatesAutoresizingMaskIntoConstraints = false
         etiqueta.text = "Fabricantes de cerveza"
-        etiqueta.font = UIFont(name: "Chalkduster", size: 20.0)
+        etiqueta.font = UIFont(name: "Chalkduster", size: 40.0)
         etiqueta.textColor = .systemBrown
         etiqueta.textAlignment = .center
     }
     
     func prepararConstraints() {
-        constraints.append(etiqueta.topAnchor.constraint(equalTo: view.topAnchor, constant: 10))
-        constraints.append(etiqueta.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 108))
-        constraints.append(etiqueta.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -108))
+        constraints.append(etiqueta.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20))
+        constraints.append(etiqueta.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 108))
+        constraints.append(etiqueta.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -108))
         
         constraints.append(tv.topAnchor.constraint(equalTo: etiqueta.bottomAnchor, constant: 20))
-        constraints.append(tv.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20))
-        constraints.append(tv.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20))
+        constraints.append(tv.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20))
+        constraints.append(tv.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20))
         
         constraints.append(botonAdd.topAnchor.constraint(equalTo: tv.bottomAnchor, constant: 20))
-        constraints.append(botonAdd.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10))
-        constraints.append(botonAdd.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20))
+        constraints.append(botonAdd.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20))
+        constraints.append(botonAdd.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20))
         
         constraints.append(botonDelete.topAnchor.constraint(equalTo: tv.bottomAnchor, constant: 20))
-        constraints.append(botonDelete.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10))
-        constraints.append(botonDelete.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20))
+        constraints.append(botonDelete.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20))
+        constraints.append(botonDelete.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20))
         constraints.append(botonDelete.centerYAnchor.constraint(equalTo: botonAdd.centerYAnchor))
         constraints.append(botonDelete.widthAnchor.constraint(equalTo: botonAdd.widthAnchor))
+        constraints.append(botonDelete.heightAnchor.constraint(equalTo: botonAdd.heightAnchor))
     }
     
     @objc func botonAnadir(_ sender : UIButton) {
-        etiqueta.text = "Has pulsado el botón de Añadir Fabricante"
+//        etiqueta.text = "Has pulsado el botón de Añadir Fabricante"
     }
     
     @objc func botonEliminar(_ sender : UIButton) {
-        etiqueta.text = "Has pulsado el botón de Eliminar Fabricante"
+//        etiqueta.text = "Has pulsado el botón de Eliminar Fabricante"
     }
 }
 
---
+++ AppCerveza/Controllers/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  AppCerveza
+//
+//  Created by María González García on 20/12/21.
+//
+// THIS IS THE MASTER BRANCH
+//
+
+import UIKit
+
+/*
+ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ <#code#>
+ }
+ 
+ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ <#code#>
+ }
+ 
+class ThirdViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+}
+*/
